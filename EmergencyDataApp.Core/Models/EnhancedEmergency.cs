fd1758d0fa38b3e176d327e674f124f32f8f3b02
Daxton Burgon <daxtonburgon@gmail.com>
2021-11-18 18:32:16 -0700

    Added new constructor for transorming Emergency to EnhancedEmergency

--- EmergencyDataApp.Core/Models/EnhancedEmergency.cs
+++ EmergencyDataApp.Core/Models/EnhancedEmergency.cs
@@ -9,5 +9,20 @@ namespace EmergencyDataApp.Core
         /// Weather data at hour time of emergency
         /// </summary>
         public Weather Weather { get; set; }
+
+        public EnhancedEmergency() { }
+
+        /// In production, I would probably take the time to setup and auto mapper
+        public EnhancedEmergency(Emergency emergency, Weather weather)
+        {
+            // In production, I would do a deep copy to keep references being shared between
+            // the two objects
+            foreach (var prop in typeof(Emergency).GetProperties())
+            {
+                prop.SetValue(this, prop.GetValue(emergency));
+            }
+
+            Weather = weather;
+        }
     }
 }
