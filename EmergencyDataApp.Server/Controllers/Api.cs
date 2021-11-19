a539ecf0724c286adab1deab0b7d1c0ff15ca8b5
Daxton Burgon <daxtonburgon@gmail.com>
2021-11-18 18:46:13 -0700

    Created working API

---
+++ EmergencyDataApp.Server/Controllers/Api.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmergencyDataApp.Core;
+using EmergencyDataApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmergencyDataApp.Server
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ApiController : Controller
+    {
+        private readonly IRepository _repository;
+
+        public ApiController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        /// <summary>
+        /// Returns all Emergencies within date range
+        /// </summary>
+        /// <param name="fromDate">From date</param>
+        /// <param name="toDate">To date</param>
+        [HttpGet("{fromDate}/{toDate}")]
+        public async Task<IActionResult> Emergency(DateTime fromDate, DateTime toDate)
+            => Ok(await _repository.GetEmergenciesAsync(fromDate, toDate));
+
+        /// <summary>
+        /// Returns weather for all hours of date
+        /// </summary>
+        /// <param name="date">Date</param>
+        [HttpGet("{date}")]
+        public async Task<IActionResult> Weather(DateTime date)
+            => Ok(await _repository.GetWeatherForDateAsync(date));
+
+        /// <summary>
+        /// Returns all emergencies with weather information for date
+        /// </summary>
+        /// <param name="fromDate">From date</param>
+        /// <param name="toDate">To date</param>
+        /// <returns></returns>
+        [HttpGet("{fromDate}/{toDate}")]
+        public async Task<IActionResult> EnhancedEmergency(DateTime fromDate, DateTime toDate)
+            => Ok(await _repository.GetEnhancedEmergenciesAsync(fromDate, toDate));
+    }
+}
