a4eedd3158bd4be47f38b00004582e3314ef7131
Daxton Burgon <daxtonburgon@gmail.com>
2021-11-18 18:45:41 -0700

    Updated repository to enclude enhanced emergencies

--- EmergencyDataApp.Data/Repository/IRespository.cs
+++ EmergencyDataApp.Data/Repository/IRespository.cs
@@ -19,9 +19,18 @@ namespace EmergencyDataApp.Data
         public Task<IEnumerable<Emergency>> GetEmergenciesAsync(DateTime fromDate, DateTime toDate);
 
         /// <summary>
+        /// Retreives emergencies with weather information
+        /// </summary>
+        /// <param name="fromDate">Filter start date</param>
+        /// <param name="toDate">Filter end date</param>
+        /// <returns>Collection of EnhancedEmergencies for date</returns>
+        public Task<IEnumerable<EnhancedEmergency>> GetEnhancedEmergenciesAsync(DateTime fromDate, DateTime toDate);
+
+        /// <summary>
         /// Retrieves weather information for date
         /// </summary>
         /// <param name="date">Desired weather data date</param>
+        /// <returns>Collection of Weather for date</returns>
         public Task<IEnumerable<Weather>> GetWeatherForDateAsync(DateTime date);
     }
 }
